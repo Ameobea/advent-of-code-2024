daea5b4d852d7f821375129c471154c1a982fa96
Casey Primozic <casey@cprimozic.net>
2024-12-03 03:01:08 -0800

    Further optimize day 3

--- benches/day3.rs
+++ benches/day3.rs
@@ -1,7 +1,10 @@
+use std::time::Duration;
+
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn day3_bench(c: &mut Criterion) {
   let mut group = c.benchmark_group("day3");
+  group.measurement_time(Duration::new(10, 0));
 
   group.bench_function("part2", |b| {
     b.iter(|| aoc_2024::day3::parse_and_compute::<true>())
--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::simd::{cmp::SimdPartialEq, u8x16};
+use std::simd::{cmp::SimdPartialEq, u8x32};
 
 use smallvec::SmallVec;
 
@@ -7,6 +7,7 @@ const INPUT: &'static [u8] = include_bytes!("../inputs/day3.txt");
 type Nums = SmallVec<[usize; 2]>;
 type Cur = SmallVec<[u8; 3]>;
 
+#[derive(Debug)]
 enum ParseState {
   Fresh,
   M,
@@ -24,7 +25,7 @@ enum ParseState {
 
 fn parse_char(c: u8) -> usize { (c - 48) as usize }
 
-fn consume_num(nums: &mut Nums, cur: Cur) {
+fn consume_num(nums: &mut Nums, cur: &Cur) {
   let num = match cur.len() {
     0 => unreachable!(),
     1 => parse_char(unsafe { *cur.get_unchecked(0) }),
@@ -35,7 +36,7 @@ fn consume_num(nums: &mut Nums, cur: Cur) {
       100 * parse_char(unsafe { *cur.get_unchecked(0) })
         + 10 * parse_char(unsafe { *cur.get_unchecked(1) })
         + parse_char(unsafe { *cur.get_unchecked(2) }),
-    _ => unreachable!(),
+    other => unreachable!("Unexpected digit count: {other}"),
   };
   nums.push(num);
 }
@@ -86,7 +87,7 @@ impl ParseState {
           if cur.is_empty() {
             Self::Fresh
           } else {
-            consume_num(&mut nums, cur);
+            consume_num(&mut nums, &cur);
             ParseState::Nums {
               nums,
               cur: SmallVec::new(),
@@ -96,9 +97,8 @@ impl ParseState {
           if cur.is_empty() {
             Self::Fresh
           } else {
-            consume_num(&mut nums, cur);
+            consume_num(&mut nums, &cur);
             if *do_state {
-              // *sum += nums[0] * nums[1];
               *sum += unsafe { nums.get_unchecked(0) * nums.get_unchecked(1) };
             }
             Self::Fresh
@@ -154,6 +154,12 @@ impl ParseState {
   }
 }
 
+const MUL: [u8; 4] = ['m' as u8, 'u' as u8, 'l' as u8, '(' as u8];
+const DONT: [u8; 7] = [
+  'd' as u8, 'o' as u8, 'n' as u8, '\'' as u8, 't' as u8, '(' as u8, ')' as u8,
+];
+const DO: [u8; 4] = ['d' as u8, 'o' as u8, '(' as u8, ')' as u8];
+
 pub fn parse_and_compute<const ENABLE_DO_STATE: bool>() -> usize {
   let mut sum = 0usize;
   let mut cur = ParseState::Fresh;
@@ -163,32 +169,113 @@ pub fn parse_and_compute<const ENABLE_DO_STATE: bool>() -> usize {
   while char_ix < INPUT.len() {
     let mut c = unsafe { *INPUT.get_unchecked(char_ix) };
     if matches!(cur, ParseState::Fresh) {
-      // if we're not currently in the middle of parsing anything, scan ahead 16 chars and check to
-      // see if they're all garbage characters that can be skipped.
+      // if we're not currently in the middle of parsing anything, the only thing we can start with
+      // is `d` or `m`.
       //
-      // scanning 16 seems to be faster than 32, probably because the `mul()`s are so dense in the
-      // actual inputs that it usually fails
-      while char_ix < INPUT.len() - (16 + 1) {
+      // So, scan ahead through the input and find the first instance of either of those characters,
+      // then jump to it.
+      while char_ix < INPUT.len() - (32 + 1) {
         let vector =
-          unsafe { u8x16::from_slice(std::slice::from_raw_parts(INPUT.as_ptr().add(char_ix), 16)) };
-        let m_mask = vector.simd_eq(u8x16::splat('m' as u8));
-        let d_mask = vector.simd_eq(u8x16::splat('d' as u8));
-        let has_hit = (m_mask | d_mask).any();
-        if has_hit {
-          break;
-        }
-        char_ix += 16;
-        c = unsafe { *INPUT.get_unchecked(char_ix) };
-      }
+          unsafe { u8x32::from_slice(std::slice::from_raw_parts(INPUT.as_ptr().add(char_ix), 32)) };
 
-      // scan through any remainder garbage ahead of time as a fast-path
-      while c != 'd' as u8 && c != 'm' as u8 {
-        char_ix += 1;
-        if char_ix == INPUT.len() {
-          return sum;
-        }
+        let m_mask = vector.simd_eq(u8x32::splat('m' as u8));
+        let combined_mask = if ENABLE_DO_STATE {
+          let d_mask = vector.simd_eq(u8x32::splat('d' as u8));
+          m_mask | d_mask
+        } else {
+          m_mask
+        };
+        let hit_ix = match combined_mask.first_set() {
+          Some(hit_ix) => hit_ix,
+          None => {
+            char_ix += 32;
+            continue;
+          },
+        };
+
+        char_ix += hit_ix;
         c = unsafe { *INPUT.get_unchecked(char_ix) };
+
+        if c == 'm' as u8 {
+          if INPUT.get(char_ix..char_ix + MUL.len()) == Some(&MUL) {
+            cur = ParseState::Nums {
+              nums: SmallVec::new(),
+              cur: SmallVec::new(),
+            };
+            char_ix += MUL.len();
+
+            if !do_state {
+              continue;
+            }
+
+            c = unsafe { *INPUT.get_unchecked(char_ix) };
+
+            // try to fastpath consume nums
+            let mut nums = Nums::default();
+            let mut cur = Cur::default();
+
+            loop {
+              if c >= '0' as u8 && c <= '9' as u8 {
+                cur.push(c);
+              } else if c == ',' as u8 {
+                if cur.is_empty() {
+                  break;
+                }
+                consume_num(&mut nums, &cur);
+                cur = Default::default();
+              } else if c == ')' as u8 {
+                if cur.is_empty() {
+                  break;
+                } else {
+                  consume_num(&mut nums, &cur);
+                  debug_assert_eq!(nums.len(), 2);
+                  sum += unsafe { nums.get_unchecked(0) * nums.get_unchecked(1) };
+
+                  char_ix += 1;
+                  if char_ix >= INPUT.len() {
+                    return sum;
+                  }
+                  c = unsafe { *INPUT.get_unchecked(char_ix) };
+                  break;
+                }
+              } else {
+                break;
+              }
+
+              char_ix += 1;
+              if char_ix >= INPUT.len() {
+                return sum;
+              }
+              c = unsafe { *INPUT.get_unchecked(char_ix) };
+            }
+
+            continue;
+          } else {
+            char_ix += 1;
+            continue;
+          }
+        } else if ENABLE_DO_STATE && c == 'd' as u8 {
+          if do_state && INPUT.get(char_ix + 1..char_ix + DONT.len()) == Some(&DONT[1..]) {
+            do_state = false;
+            char_ix += DONT.len();
+            continue;
+          } else if !do_state && INPUT.get(char_ix..char_ix + DO.len()) == Some(&DO) {
+            do_state = true;
+            char_ix += DO.len();
+            continue;
+          } else {
+            char_ix += 1;
+            c = unsafe { *INPUT.get_unchecked(char_ix) };
+            continue;
+          }
+        }
+
+        break;
       }
+    }
+
+    if char_ix >= INPUT.len() {
+      break;
     }
 
     cur = cur.next::<ENABLE_DO_STATE>(&mut sum, &mut do_state, c);
