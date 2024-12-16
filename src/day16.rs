5a3d97f4e5b7e94e4a4c8aa9c65d01430a4aace5
Casey Primozic <me@ameo.link>
2024-12-15 23:26:16 -0800

    Clean up + slightly optimize day16

--- src/day16.rs
+++ src/day16.rs
@@ -1,12 +1,21 @@
 use std::sync::{Arc, Mutex};
 
 use fxhash::FxHashSet;
+use pathfinding::directed::astar::astar;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 pub const INPUT: &str = include_str!("../inputs/day16.txt");
 
 fn parse_input(input: &str) -> Vec<Vec<char>> {
   input.lines().map(|l| l.chars().collect()).collect()
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd, Debug)]
+enum Dir {
+  U,
+  D,
+  L,
+  R,
 }
 
 pub fn solve() {
@@ -24,57 +33,30 @@ pub fn solve() {
     }
   }
 
-  #[derive(Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd, Debug)]
-  enum Dir {
-    U,
-    D,
-    L,
-    R,
+  fn successors<'a>(
+    grid: &'a [Vec<char>],
+    &(dir, (x, y)): &(Dir, (usize, usize)),
+  ) -> impl Iterator<Item = ((Dir, (usize, usize)), usize)> + 'a {
+    [
+      (Dir::L, [x - 1, y]),
+      (Dir::R, [x + 1, y]),
+      (Dir::U, [x, y - 1]),
+      (Dir::D, [x, y + 1]),
+    ]
+    .into_iter()
+    .filter_map(move |(o_dir, [o_x, o_y])| {
+      // no need for bounds checking because the maze is always surrounded by a wall
+      let o = grid[y][x];
+      if o == '#' {
+        return None;
+      }
+      Some(((o_dir, (o_x, o_y)), if o_dir == dir { 1 } else { 1001 }))
+    })
   }
 
-  let path = pathfinding::directed::astar::astar(
+  let path = astar(
     &(Dir::R, start),
-    |&(dir, (x, y))| {
-      let mut outs = Vec::new();
-
-      for (o_dir, [o_x, o_y]) in [
-        (Dir::L, [x as isize - 1, y as isize]),
-        (Dir::R, [x as isize + 1, y as isize]),
-        (Dir::U, [x as isize, y as isize - 1]),
-        (Dir::D, [x as isize, y as isize + 1]),
-      ] {
-        if o_x < 0 || o_y < 0 || o_x >= grid[0].len() as _ || o_y >= grid.len() as _ {
-          continue;
-        }
-
-        if o_dir == Dir::L && dir == Dir::R {
-          continue;
-        }
-        if o_dir == Dir::R && dir == Dir::L {
-          continue;
-        }
-        if o_dir == Dir::D && dir == Dir::U {
-          continue;
-        }
-        if o_dir == Dir::U && dir == Dir::D {
-          continue;
-        }
-
-        let o = grid[y][x];
-        if o == '#' {
-          continue;
-        }
-        outs.push((
-          o_dir,
-          (o_x as usize, o_y as usize),
-          if o_dir == dir { 1 } else { 1001 },
-        ));
-      }
-
-      outs
-        .into_iter()
-        .map(|(dir, coord, cost)| ((dir, coord), cost))
-    },
+    |c| successors(&grid, c),
     |_| 1,
     |c| c.1 == end,
   )
@@ -91,58 +73,9 @@ pub fn solve() {
     .into_par_iter()
     .flat_map(|y| (0..width).into_par_iter().map(move |x| (x, y)))
     .for_each(move |(x, y)| {
-      let path = pathfinding::directed::astar::astar(
+      let path = astar(
         &(Dir::R, start),
-        |&(dir, (x, y))| {
-          let mut outs = Vec::new();
-          let (turn_dirs, turn_cost) = match dir {
-            Dir::U => (vec![Dir::L, Dir::R], 1000),
-            Dir::D => (vec![Dir::L, Dir::R], 1000),
-            Dir::L => (vec![Dir::U, Dir::D], 1000),
-            Dir::R => (vec![Dir::U, Dir::D], 1000),
-          };
-          for turn_dir in turn_dirs {
-            outs.push((turn_dir, (x, y), turn_cost));
-          }
-
-          for (o_dir, [o_x, o_y]) in [
-            (Dir::L, [x as isize - 1, y as isize]),
-            (Dir::R, [x as isize + 1, y as isize]),
-            (Dir::U, [x as isize, y as isize - 1]),
-            (Dir::D, [x as isize, y as isize + 1]),
-          ] {
-            if o_x < 0 || o_y < 0 || o_x >= grid[0].len() as _ || o_y >= grid.len() as _ {
-              continue;
-            }
-
-            if o_dir == Dir::L && dir == Dir::R {
-              continue;
-            }
-            if o_dir == Dir::R && dir == Dir::L {
-              continue;
-            }
-            if o_dir == Dir::D && dir == Dir::U {
-              continue;
-            }
-            if o_dir == Dir::U && dir == Dir::D {
-              continue;
-            }
-
-            let o = grid[y][x];
-            if o == '#' {
-              continue;
-            }
-            outs.push((
-              o_dir,
-              (o_x as usize, o_y as usize),
-              if o_dir == dir { 1 } else { 1001 },
-            ));
-          }
-
-          outs
-            .into_iter()
-            .map(|(dir, coord, cost)| ((dir, coord), cost))
-        },
+        |c| successors(&grid, c),
         |_| 1,
         |c| c.1 == (x, y),
       );
@@ -155,58 +88,9 @@ pub fn solve() {
         return;
       }
 
-      let path = pathfinding::directed::astar::astar(
+      let path = astar(
         path.0.last().unwrap(),
-        |&(dir, (x, y))| {
-          let mut outs = Vec::new();
-          let (turn_dirs, turn_cost) = match dir {
-            Dir::U => (vec![Dir::L, Dir::R], 1000),
-            Dir::D => (vec![Dir::L, Dir::R], 1000),
-            Dir::L => (vec![Dir::U, Dir::D], 1000),
-            Dir::R => (vec![Dir::U, Dir::D], 1000),
-          };
-          for turn_dir in turn_dirs {
-            outs.push((turn_dir, (x, y), turn_cost));
-          }
-
-          for (o_dir, [o_x, o_y]) in [
-            (Dir::L, [x as isize - 1, y as isize]),
-            (Dir::R, [x as isize + 1, y as isize]),
-            (Dir::U, [x as isize, y as isize - 1]),
-            (Dir::D, [x as isize, y as isize + 1]),
-          ] {
-            if o_x < 0 || o_y < 0 || o_x >= grid[0].len() as _ || o_y >= grid.len() as _ {
-              continue;
-            }
-
-            if o_dir == Dir::L && dir == Dir::R {
-              continue;
-            }
-            if o_dir == Dir::R && dir == Dir::L {
-              continue;
-            }
-            if o_dir == Dir::D && dir == Dir::U {
-              continue;
-            }
-            if o_dir == Dir::U && dir == Dir::D {
-              continue;
-            }
-
-            let o = grid[y][x];
-            if o == '#' {
-              continue;
-            }
-            outs.push((
-              o_dir,
-              (o_x as usize, o_y as usize),
-              if o_dir == dir { 1 } else { 1001 },
-            ));
-          }
-
-          outs
-            .into_iter()
-            .map(|(dir, coord, cost)| ((dir, coord), cost))
-        },
+        |c| successors(&grid, c),
         |_| 1,
         |c| c.1 == end,
       );
