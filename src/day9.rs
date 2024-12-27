758649fd7b7f8f7fe00dd9d2926a757b78d5af08
Casey Primozic <casey@cprimozic.net>
2024-12-27 16:47:42 -0600

    better input parsing

--- src/day9.rs
+++ src/day9.rs
@@ -5,21 +5,32 @@ use std::fmt::Display;
 #[cfg(feature = "local")]
 pub const INPUT: &'static [u8] = include_bytes!("../inputs/day9.txt");
 
-fn parse_digit(c: u8) -> usize { (c - 48) as usize }
+fn parse_digit(c: u8) -> usize { (c.saturating_sub(48)) as usize }
 
-fn parse_input(input: &[u8]) -> Vec<(usize, usize)> {
-  let mut it = input[..input.len() - if input.len() % 2 == 0 { 1 } else { 0 }].array_chunks::<2>();
+fn parse_input(input: &[u8]) -> (Vec<usize>, Vec<MiniVec>, Vec<usize>) {
+  let mut it = input.array_chunks::<2>();
 
-  let mut out = Vec::with_capacity(20_002 / 2);
+  let mut orig_counts = Vec::with_capacity(input.len() / 2);
+  let mut slots: Vec<MiniVec> = Vec::with_capacity(input.len() / 2);
+  unsafe { slots.set_len(input.len() / 2) };
+  let mut empty_spaces = Vec::with_capacity(input.len());
+
+  let mut id = 0usize;
   while let Some(&[size, free]) = it.next() {
-    out.push((parse_digit(size), parse_digit(free)));
+    let size = parse_digit(size);
+    let free = parse_digit(free);
+    orig_counts.push(size as usize);
+    slots[id].len = 1;
+    slots[id].elements[0] = Slot {
+      count: size as _,
+      id,
+    };
+    empty_spaces.push(free as usize);
+    id += 1;
   }
+  empty_spaces.push(0);
 
-  if let Some(remainder) = it.remainder().get(0) {
-    out.push((parse_digit(*remainder), 0));
-  }
-
-  out
+  (orig_counts, slots, empty_spaces)
 }
 
 fn compute_fs(input: &[(usize, usize)]) -> Vec<Option<usize>> {
@@ -36,99 +47,94 @@ fn compute_fs(input: &[(usize, usize)]) -> Vec<Option<usize>> {
   fs
 }
 
-pub fn part1(input: &[u8]) -> usize {
-  let input = parse_input(input);
-  let mut fs = compute_fs(&input);
+const ADD_FACTORIAL_LUT: [usize; 11] = [
+  0,
+  0,
+  1,
+  2 + 1,
+  3 + 2 + 1,
+  4 + 3 + 2 + 1,
+  5 + 4 + 3 + 2 + 1,
+  6 + 5 + 4 + 3 + 2 + 1,
+  7 + 6 + 5 + 4 + 3 + 2 + 1,
+  8 + 7 + 6 + 5 + 4 + 3 + 2 + 1,
+  9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1,
+];
 
-  let mut dst_ix = 0usize;
-  for src_ix in (0..fs.len()).rev() {
-    let Some(id) = fs[src_ix] else { continue };
+#[derive(Clone, Copy, Debug)]
+struct Slot {
+  pub id: usize,
+  pub count: usize,
+}
 
-    if dst_ix >= src_ix {
-      break;
+impl Slot {
+  fn checksum(&self, total_prev: usize) -> usize {
+    // naive impl:
+    // (0..self.count)
+    //   .map(|i| (total_prev + i as usize) * self.id as usize)
+    //   .sum::<usize>()
+    // So, this condenses down to a sum of the following:
+    //
+    // (total_prev + 0) * id
+    // (total_prev + 1) * id
+    // (total_prev + 2) * id
+    // ...
+    // (total_prev + (count - 1)) * id
+    //
+    // the `total_prev` part can be split out:
+    // total_prev * self.count * id
+    //
+    // leaving that base plus a sum of the following:
+    //
+    // 0 * id
+    // 1 * id
+    // 2 * id
+    // ...
+    // (count - 1) * id
+    //
+    // this reduces to (0 + 1 + 2 + ... + (count - 1)) * id
+    //
+    // and since count is always [0,9], we can use a tiny LUT for this which makes this whole
+    // checksum essentially constant time
+    total_prev * self.count as usize * self.id as usize
+      + unsafe { *ADD_FACTORIAL_LUT.get_unchecked(self.count as usize) } * self.id as usize
+  }
+}
+
+#[derive(Clone, Debug)]
+struct MiniVec {
+  pub len: u32,
+  pub elements: [Slot; 6],
+}
+
+impl MiniVec {
+  fn push(&mut self, item: Slot) {
+    unsafe {
+      *self.elements.get_unchecked_mut(self.len as usize) = item;
     }
+    self.len += 1;
+    debug_assert!(self.len as usize <= self.elements.len());
+  }
 
-    while fs[dst_ix].is_some() {
-      dst_ix += 1;
-      if dst_ix >= src_ix {
-        break;
+  fn pop_front(&mut self) {
+    for i in 1..self.len {
+      unsafe {
+        *self.elements.get_unchecked_mut(i as usize - 1) = self.elements[i as usize];
       }
     }
-
-    fs[dst_ix] = Some(id);
-    fs[src_ix] = None;
-    dst_ix += 1;
-    if dst_ix >= src_ix {
-      break;
-    }
+    self.len -= 1;
+    return;
+    // we should only ever mutate the vector once
+    debug_assert!(self.elements[0].count != 0);
+    // this is a nice trick I came up with to accomplish the equivalent
+    self.elements[0].count = 0;
   }
 
-  let mut out = 0usize;
-  for i in 0..fs.len() {
-    let Some(id) = fs[i] else {
-      continue;
-    };
-    out += i * id;
-  }
-
-  out
+  fn as_slice(&self) -> &[Slot] { unsafe { self.elements.get_unchecked(..self.len as usize) } }
 }
 
 pub fn part2(input: &[u8]) -> usize {
-  let input = parse_input(input);
-
-  let mut data_start_pos = Vec::new();
-  let mut cur_start_pos = 0usize;
-  for i in 0..input.len() {
-    data_start_pos.push(cur_start_pos);
-    cur_start_pos += input[i].0 + input[i].1;
-  }
-
-  #[derive(Clone, Copy, Debug)]
-  struct Slot {
-    pub id: usize,
-    pub count: usize,
-  }
-
-  impl Slot {
-    fn checksum(&self, total_prev: usize) -> usize {
-      (0..self.count)
-        .map(|i| (total_prev + i) * self.id)
-        .sum::<usize>()
-    }
-  }
-
-  #[derive(Clone, Debug)]
-  struct MiniVec {
-    pub len: u32,
-    pub elements: [Slot; 6],
-  }
-
-  impl MiniVec {
-    fn push(&mut self, item: Slot) {
-      unsafe {
-        *self.elements.get_unchecked_mut(self.len as usize) = item;
-      }
-      self.len += 1;
-      debug_assert!(self.len as usize <= self.elements.len());
-    }
-
-    fn pop_front(&mut self) {
-      for i in 1..self.len {
-        unsafe {
-          *self.elements.get_unchecked_mut(i as usize - 1) = self.elements[i as usize];
-        }
-      }
-      self.len -= 1;
-      return;
-      // we should only ever mutate the vector once
-      debug_assert!(self.elements[0].count != 0);
-      // this is a nice trick I came up with to accomplish the equivalent
-      self.elements[0].count = 0;
-    }
-
-    fn as_slice(&self) -> &[Slot] { unsafe { self.elements.get_unchecked(..self.len as usize) } }
-  }
+  let (input, mut slots, mut empty_spaces) = parse_input(input);
 
   fn checksum(total_prev: &mut usize, slots: &[Slot], empty_space: usize) -> usize {
     debug_assert!(slots.len() <= 4);
@@ -142,18 +148,9 @@ pub fn part2(input: &[u8]) -> usize {
     sum
   }
 
-  let mut slots: Vec<MiniVec> = Vec::with_capacity(input.len());
-  unsafe { slots.set_len(input.len()) };
-  let mut empty_spaces = Vec::with_capacity(input.len());
-  for (id, &(count, free)) in input.iter().enumerate() {
-    slots[id].len = 1;
-    slots[id].elements[0] = Slot { count, id };
-    empty_spaces.push(free);
-  }
-
   let mut start_span_ix_by_needed_size: [usize; 10] = [0; 10];
   for src_id in (0..input.len()).rev() {
-    let (src_count, _) = input[src_id];
+    let src_count = input[src_id];
 
     let start_ix = start_span_ix_by_needed_size[src_count];
     if start_ix >= src_id {
@@ -210,9 +207,9 @@ pub fn part2(input: &[u8]) -> usize {
 
 #[cfg(feature = "local")]
 pub fn solve() {
-  let out = part1(INPUT);
+  // let out = part1(INPUT);
 
-  println!("Part 1: {}", out);
+  // println!("Part 1: {}", out);
 
   let out = part2(INPUT);
 
